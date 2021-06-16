cc904fe0a0f5ab85f162ba0026b66709dc113e22
Sriram Sridharan <s.sriram@yethi.in>
2021-06-16 19:20:04 +0530

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import MarketingApp from "./components/MarketingApp";
+
+const App = () => {
+  return (
+    <div>
+      <p>Hi there!</p>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
+
+export default App;
