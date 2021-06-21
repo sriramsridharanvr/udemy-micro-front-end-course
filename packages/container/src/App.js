14a185e47e2b67f234314f65798dfd57c9d14cfa
Sriram Sridharan <s.sriram@yethi.in>
2021-06-21 20:01:25 +0530

    Added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
-
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <div>
-        <p>Hi there!!!!</p>
-      </div>
-      <hr />
-      <div>
+        <Header />
         <MarketingApp />
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
